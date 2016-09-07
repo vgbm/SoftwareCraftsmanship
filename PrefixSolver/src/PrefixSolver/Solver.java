57d2b5a6d1599b97a59cc1f75dca7fb7e392c5c3
James <james.thomas.vaughn@gmail.com>
2016-09-07 11:26:12 -0400

    Pulled out some functions

--- PrefixSolver/src/PrefixSolver/Solver.java
+++ PrefixSolver/src/PrefixSolver/Solver.java
@@ -11,19 +11,26 @@ import java.util.List;
 public class Solver {
 
     public static <T> List<T> longestPrefix(List<T> list1, List<T> list2, Comparator<? super T> cmp){
-        Iterator iter1 = list1.iterator();
-        Iterator iter2 = list2.iterator();
+        Iterator<T> iter1 = list1.iterator();
+        Iterator<T> iter2 = list2.iterator();
 
+        return findCommonPrefix(iter1, iter2, cmp);
+    }
+
+    private static <T> boolean bothIteratorsHaveNextElem(Iterator<T> iter1, Iterator<T> iter2){
+       return iter1.hasNext() && iter2.hasNext();
+    }
+
+    private static <T> List<T> findCommonPrefix(Iterator<T> iter1, Iterator<T> iter2, Comparator<? super T> cmp) {
         //ArrayList is preferred to LinkedList since appending to lists should be O(1)
         List<T> prefix = new ArrayList<>();
 
-        while( iter1.hasNext() && iter2.hasNext() ) {
-            Object elem = iter1.next();
+        while( bothIteratorsHaveNextElem(iter1, iter2) ) {
+            T elem = iter1.next();
             if(cmp.compare(elem, iter2.next()) == 0) {
                 prefix.add(elem);
             }
         }
 
-        return prefix;
     }
 }
