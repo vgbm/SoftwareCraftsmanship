4e1da160966660b14388f82ec6699cf1add30cce
James <james.thomas.vaughn@gmail.com>
2016-10-18 21:13:41 -0400

    Have working solution; must reduce complexity

---
+++ Spy/src/src/Spy.java
@@ -0,0 +1,94 @@
+import java.util.*;
+
+/**
+ * Created by james on 10/18/16.
+ */
+public class Spy <T> {
+
+
+    private HashMap<T, HashSet<T>> knownObjectsToOpponents;
+
+    public Spy(){
+        knownObjectsToOpponents = new HashMap<>();
+    }
+
+    public boolean create(T x) {
+        if( !knownObjectsToOpponents.containsKey(x) ) {
+            knownObjectsToOpponents.put(x, new HashSet<>());
+            return true;
+        }
+        return false;
+    }
+
+    public void oppose(T x, T y) throws RuntimeException {
+        //needs to be a bitwise OR to prevent short-circuiting
+        boolean hadToCreateXOrY = create(x) | create(y);
+
+
+        if( !hadToCreateXOrY && areNotOpponents(x, y)) {
+            throw new RuntimeException("X and Y cannot be made opponents.");
+        }
+
+        HashSet<T> xHashSet = knownObjectsToOpponents.get(x);
+        HashSet<T> yHashSet = knownObjectsToOpponents.get(y);
+
+        xHashSet.add(y);
+        yHashSet.add(x);
+    }
+
+    private boolean areNotOpponents(T x, T y) {
+        //Not using orElse to reduce the optional checking as the result of doing so looks really confusing
+        //It looks like this: areNotOpponents = !opponents(x, y).orElse(true);
+        //I find it hard to keep the flag for orElse straight in my head, so I would rather be explicit here
+        //at the cost of additional checking and complexity
+        Optional<Boolean> areOpponents = opponents(x, y);
+        return areOpponents.isPresent() && !areOpponents.get();
+    }
+    public Optional<Boolean> opponents(T x, T y){
+        int depth = depthToY(x, y);
+
+        if (depth == -1) {
+            return Optional.empty();
+        }
+        //this looks silly since it should just be Optional.of(depth%2==1), but I will stick to the pseudocode
+        else if (depth % 2 == 1) {
+            return Optional.of(true);
+        }
+        return Optional.of(false);
+    }
+
+    private int depthToY(T x, T y) {
+        boolean wasFound = false;
+        int depth = 0;
+
+        ArrayDeque<T> searchQueue = new ArrayDeque<T>();
+        Set<T> inspectedObjects = new HashSet<T>();
+
+        searchQueue.add(x);
+
+        breadthFirstSearchLoop:
+        while ( !searchQueue.isEmpty() ) {
+            depth++;
+
+            T current = searchQueue.poll();
+            if(knownObjectsToOpponents.get(current).contains(y)) {
+                wasFound = true;
+                break breadthFirstSearchLoop;
+            }
+
+            for (T neighbor : knownObjectsToOpponents.get(current)) {
+                boolean needsToBeChecked = inspectedObjects.add(neighbor);
+                if( needsToBeChecked) {
+                    searchQueue.add(neighbor);
+                }
+            }
+        }
+
+        return wasFound? depth : -1;
+    }
+
+    //delegate for testing purposes
+    public boolean containsKey(Object o) {
+        return knownObjectsToOpponents.containsKey(o);
+    }
+}
---
+++ Spy/src/src/SpyTest.java
@@ -0,0 +1,102 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by james on 10/18/16.
+ */
+public class SpyTest {
+
+    //using integer objects to test, since we need objects
+    private static final Spy<Integer> _spy = new Spy<>();
+    private static final Integer A = 1,
+                                B = 2,
+                                C = 3,
+                                D = 4,
+                                E = 5;
+
+
+    public static class CreateTester {
+
+        @Test
+        public void Should_add_new_key_and_return_true() {
+            boolean didCreate = _spy.create(A);
+            assertTrue(didCreate);
+            assertTrue(_spy.containsKey(A));
+        }
+
+        @Test
+        public void Should_return_false_when_creating_existing_key() {
+            _spy.create(A);
+            boolean didCreate = _spy.create(A);
+            assertFalse(didCreate);
+        }
+
+    }
+
+    public static class OpposeTester {
+
+        @Before
+        public void SetUp() {
+            _spy.create(E);
+            _spy.oppose(A,B);
+            _spy.oppose(B,C);
+            _spy.oppose(C,D);
+        }
+
+        @Test
+        public void Should_create_entry_for_new_objects(){
+            _spy.create(A);
+            assertFalse(_spy.containsKey(B));
+            _spy.oppose(A,B);
+            assertTrue(_spy.containsKey(B));
+        }
+
+        @Test
+        public void Should_add_objects_as_opponents_bidirectionally(){
+            _spy.oppose(A,B);
+            assertTrue(_spy.opponents(A,B).get());
+            assertTrue(_spy.opponents(B,A).get());
+        }
+
+        @Test (expected=RuntimeException.class)
+        public void Should_error_if_objects_are_not_opponents(){
+            _spy.oppose(A,B);
+            _spy.oppose(B,C);
+            _spy.oppose(A,C);
+        }
+    }
+
+    public static class OpponentTester {
+
+        @Before
+        public void SetUp() {
+            _spy.create(E);
+            _spy.oppose(A,B);
+            _spy.oppose(B,C);
+            _spy.oppose(C,D);
+        }
+
+        @Test
+        public void Should_return_empty_optional_if_no_connection_exists(){
+            assertFalse(_spy.opponents(A,E).isPresent());
+        }
+
+        @Test
+        public void Should_be_opponents_when_direct_opponents(){
+            assertTrue(_spy.opponents(A,B).get());
+        }
+
+        @Test
+        public void Should_be_opponents_when_depth_to_object_is_odd(){
+            assertFalse(_spy.opponents(A,D).get());
+        }
+
+        @Test
+        public void Should_not_be_opponents_when_depth_to_object_is_even(){
+            assertFalse(_spy.opponents(A,C).get());
+        }
+    }
+}
